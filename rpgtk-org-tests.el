d7e833e2c066f2bcef7191d446283a9212195262
Howard Abrams <howard.abrams@gmail.com>
2023-11-12 09:24:51 -0800

    Read property values from Org Files specific for RPGTK
    Got plans for full CRUD interface.

---
+++ rpgtk-org-tests.el
@@ -0,0 +1,90 @@
+;;; rpgtk-org-tests.el --- unit tests for rpgtk-org  -*- lexical-binding: t; -*-
+;;
+;; © 2023 Howard X. Abrams
+;;   Licensed under a Creative Commons Attribution 4.0 International License.
+;;   See http://creativecommons.org/licenses/by/4.0/
+;;
+;; Author: Howard X. Abrams <http://gitlab.com/howardabrams>
+;; Maintainer: Howard X. Abrams
+;; Created: 11 November 2023
+;;
+;; Obviously, GNU Emacs does not include this file in its distribution.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Unit tests for functions in the `rpgtk-org.el' using ERT.
+
+;;; Code:
+
+(require 'rpgtk-org)
+
+(ert-deftest rpgtk-org--property-key-test ()
+  (should (eq :FOO-BAR (rpgtk-org--property-key 'FOO 'BAR)))
+  (should (eq :FOO-BAR (rpgtk-org--property-key 'foo 'bar)))
+  (should (eq :FOO-BAR (rpgtk-org--property-key "foo" "bar")))
+  (should (eq :RPG-BAR (rpgtk-org--property-key nil "bar"))))
+
+(ert-deftest rpgtk-org--property-value-test ()
+  (should (= 42 (rpgtk-org--property-value "42")))
+  (should (equal '("foo" "bar") (rpgtk-org--property-value "foo bar")))
+  (should (equal "foobar" (rpgtk-org--property-value "foobar"))))
+
+(ert-deftest rpgtk-org--string-to-list-test ()
+  (should (null (rpgtk-org--string-to-list nil)))
+  (should (null (rpgtk-org--string-to-list "")))
+
+  ;; Single elements:
+  (should (equal '("foo bar") (rpgtk-org--string-to-list "\"foo bar\"")))
+  (should (equal '(42) (rpgtk-org--string-to-list "42")))
+  (should (equal '(bob) (rpgtk-org--string-to-list ":bob")))
+  (should (equal '("bob") (rpgtk-org--string-to-list "bob")))
+
+  ;; Multiple elements:
+  (should (equal (list "foo" "bar")
+                 (rpgtk-org--string-to-list "foo bar")))
+  (should (equal (list "foo" "the second" "bar")
+                 (rpgtk-org--string-to-list "foo \"the second\" bar")))
+  (should (equal (list "foo" 42)
+                 (rpgtk-org--string-to-list "foo 42")))
+  (should (equal (list "one" 'two "buckle my shoe" 3 'four)
+                 (rpgtk-org--string-to-list
+                  "one :two \"buckle my shoe\" 3 :four"))))
+
+(ert-deftest rpgtk-org-read--property-test ()
+  (flet ((org-element--get-node-properties
+          ()
+          (list :RPG-FOOBAR "42"
+                :RPG-FOOLIST "one :two \"buckle my shoe\" 3 :four"
+                :IGNORED "this is ignored")))
+    (should (= 42 (rpgtk-org--read-property :RPG-FOOBAR)))
+    (should (equal '("one" two "buckle my shoe" 3 four)
+                   (rpgtk-org--read-property :RPG-FOOLIST)))))
+
+(ert-deftest rpgtk-org-read-property-test ()
+  (flet ((org-at-heading-p ()  t)
+         (org-up-heading () t)
+         (org-heading-level () 1)
+         (org-element--get-node-properties
+          ()
+          (list :RPG-FOOBAR "42"
+                :RPG-FOOLIST "one :two \"buckle my shoe\" 3 :four"
+                :IGNORED "this is ignored")))
+    (should (= 42 (rpgtk-org-read-property 'foobar 'rpg)))
+    (should (= 42 (rpgtk-org-read-property "foobar" "rpg")))
+    (should (= 42 (rpgtk-org-read-property "foobar")))))
+
+(provide 'rpgtk-org-tests)
+;;; rpgtk-org-tests.el ends here
---
+++ rpgtk-org.el
@@ -0,0 +1,197 @@
+;;; rpgtk-org.el --- embed PC stats in org files  -*- lexical-binding: t; -*-
+;;
+;; © 2023 Howard X. Abrams
+;;   Licensed under a Creative Commons Attribution 4.0 International License.
+;;   See http://creativecommons.org/licenses/by/4.0/
+;;
+;; Author: Howard X. Abrams <http://gitlab.com/howardabrams>
+;; Maintainer: Howard X. Abrams
+;; Created:  6 November 2023
+;;
+;; Obviously, GNU Emacs does not include this file in its distribution.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Helper functions for parsing property values in an org-formatted
+;; file. These functions are specifically _designed_ for storing
+;; character statistics, attributes and other meta-data, in a property
+;; drawers in an org file.
+;;
+;;   - `rpgtk-org-create-property' : creates a property value at the
+;;     top-level heading
+;;
+;;   - `rpgtk-org-set-property' : sets a property value at the current
+;;     heading level
+;;
+;;   - `rpgtk-org-read-property' : reads a property searching heading
+;;     levels until it finds it
+;;
+;;   - `rpgtk-org-update-property' : changes property at whatever
+;;     property level it is
+;;
+;;   - `rpgtk-org-adjust-property' : changes property value by a
+;;     relative, numeric value
+;;
+;;   - `rpgtk-org-delete-property' : removes the property at current
+;;     level, or in higher levels.
+;;
+;;; Code:
+
+(require 'cl)
+(require 'org)
+(require 'loop)
+(require 'org-element)
+
+;;As I've mentioned before, the code needs to walk "up" an Org Tree
+;;looking for properties. The crux is using the /internal/
+;;`org-element-get-node-properties' function, which returns a property
+;;list iff the point is on a header.
+;;
+;;So the general idea is:
+;;  - Move to the previous header
+;;  - Collect the properties
+;;  - Move up to that header's parent
+;;  - Collect its properties, etc.
+;;  - Stop if the point is at the /top-level/ header
+;;
+;;Since we need to know if we are at the top-level, we could have a
+;;function, =org-heading-level= that returns =1= if we are at the
+;;top-level, and =0= if we aren't at any level:
+
+(defun org-heading-level ()
+  "Return heading level of the element at the point.
+Return 0 if not at a heading, or above first headline."
+  (if-let ((level-str (org-element-property :level (org-element-at-point))))
+      level-str
+    0))
+
+;;Since `org-up-element' behavior has changed, and
+;;`outline-up-heading' doesn’t go to the next heading if it is already
+;;on a heading, we need to make a little helper:
+
+(defun org-up-heading ()
+  "Move the point to next parent heading, unless already at the top-level."
+  (interactive)
+  (cl-case (org-heading-level)
+    (0 (org-previous-visible-heading 1))
+    (1 nil)
+    (t (outline-up-heading 1))))
+
+;;And a function to move the point to the top-most heading (where we
+;;store the character’s details:
+
+(defun org-top-heading ()
+  "Move the point to the top-most heading in the org document.
+Note that this is based on the current point position."
+  (interactive)
+  (org-up-heading)
+  (while (> (org-heading-level) 1)
+    (org-up-heading)))
+
+;; --------------------
+
+(defvar rpgtk-org-default-property-prefix "rpg"
+  "A string that will be prefixed to all properties.")
+
+(defun rpgtk-org--property-key (prefix key)
+  "Create symbol by combining PREFIX and KEY for org properties.
+The key can be a string or symbol. If PREFIX is nil, use value of
+`rpgtk-org-default-property-prefix'."
+  (let ((pre (or prefix rpgtk-org-default-property-prefix)))
+    (thread-last key
+                 (format ":%s-%s" pre)
+                 (upcase)
+                 (intern))))
+
+(defun rpgtk-org--string-to-list (str)
+  "Convert STR to a list of elements, split on space characters.
+This recursive function looks for the following tokens:
+  * A word of digits is a number
+  * A word starting with a : is a symbol
+  * Everything else is a string
+
+Note that if you want to have a string with space, surround the
+string with single or double quotes."
+  (unless (or (null str) (string-empty-p str))
+    (let* ((dquoted? (rx bos "\""
+                        (group (1+ (not "\"")))
+                        "\""
+                        (zero-or-more space)))
+           (squoted? (rx bos "'"
+                        (group (1+ (not "'")))
+                        "'"
+                        (zero-or-more space)))
+           (number? (rx bos
+                        (group (1+ digit))
+                        (zero-or-more space)))
+           (symbol? (rx bos ":"
+                        (group (1+ (not space)))
+                        (zero-or-more space)))
+           (string? (rx bos
+                        (group (1+ (not space)))
+                        (zero-or-more space)))
+           (head   (cond
+                    ((or (string-match dquoted? str)
+                         (string-match squoted? str))
+                     (match-string 1 str))
+                    ((string-match number? str)
+                     (string-to-number (match-string 1 str)))
+                    ((string-match symbol? str)
+                     (intern (match-string 1 str)))
+                    ((string-match string? str)
+                     (match-string 1 str)))))
+      (cons head
+            (rpgtk-org--string-to-list (substring str (match-end 0)))))))
+
+(defun rpgtk-org--property-value (value)
+  "If VALUE, a string, can be a number, return VALUE as number.
+If VALUE looks like a list of items, return VALUE as list.
+Otherwise, return VALUE as a string."
+  (cond
+   ((string-match (rx bos (one-or-more digit) eos) value)
+    (string-to-number value))
+   ((string-match (rx space) value)
+    (rpgtk-org--string-to-list value))
+   (t value)))
+
+;; --------------------
+
+(defun rpgtk-org--read-property (prop-key)
+  "Helper function for `rpgtk-org-read-property'.
+Read current header properties for PROP-KEY, and return
+the value of that property."
+  (let ((props (org-element--get-node-properties)))
+    (when-let ((value (plist-get props prop-key 'equal)))
+      (rpgtk-org--property-value value))))
+
+(defun rpgtk-org-read-property (prop &optional prefix)
+  "Return first occurrence of property, PROP in org buffer.
+Note that PROP (or PREFIX) can be a string or symbol.
+If PREFIX is specified, pre-pend it to PROP, otherwise, pre-pend
+value of `rpgtk-org-default-property-prefix'."
+  (let ((key (rpgtk-org--property-key prefix prop))
+        (results nil))
+    (save-excursion
+      (unless (org-at-heading-p)
+        (org-up-heading))
+
+      (loop-until (or results (< (org-heading-level) 1))
+        (setq results (rpgtk-org--read-property key))
+        (unless results (org-up-heading)))
+      results)))
+
+(provide 'rpgtk-org)
+;;; rpgtk-org.el ends here
